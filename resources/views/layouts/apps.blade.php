1b449ae5e338b2b87191b6a2ac17c81e4a03010c
ranggadi <adirangga922@gmail.com>
2020-02-18 11:42:15 +0700

    [Rangga] feat: adding proker migration, galeri pages and galeri simple styling

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -6,8 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>HMPD FKUB</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
 </head>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 @include('inc.navbar')
 
 <body>
--- resources/views/page/galeri.blade.php
+++ resources/views/page/galeri.blade.php
@@ -2,4 +2,47 @@
 
 @section('body')
     {{-- Isi sini gan --}}
+    <section class="galeri">
+        <div class="jumbotron jumbotron-fluid galeri__image-jumbotron">
+        </div>
+        <div class="jumbotron jumbotron-fluid galeri__custom-jumbotron">
+            <div class="container galeri__custom-container">
+                <div class="galeri__photo-container">
+                    <div class="galeri__photo-line galeri__photo-line--1">
+                        <div class="galeri__photo-image">
+                            <img class="galeri__image" src="{{URL::asset('/images/dummy_galeri-1.jpg')}}" alt="galeri image">
+                        </div>
+                        <div class="galeri__photo-image">
+                            <img class="galeri__image" src="{{URL::asset('/images/dummy_galeri-2.jpg')}}" alt="">
+                        </div>
+                        <div class="galeri__photo-image">
+                            <img class="galeri__image" src="{{URL::asset('/images/dummy_galeri-3.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <div class="galeri__photo-line galeri__photo-line--2">
+                        <div class="galeri__photo-image">
+                            <img class="galeri__image" src="{{URL::asset('/images/dummy_galeri-4.jpg')}}" alt="">
+                        </div>
+                        <div class="galeri__photo-image">
+                            <img class="galeri__image" src="{{URL::asset('/images/dummy_galeri-5.jpg')}}" alt="">
+                        </div>
+                        <div class="galeri__photo-image">
+                            <img class="galeri__image" src="{{URL::asset('/images/dummy_galeri-1.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <div class="galeri__photo-line galeri__photo-line--3">
+                        <div class="galeri__photo-image">
+                            <img class="galeri__image" src="{{URL::asset('/images/dummy_galeri-2.jpg')}}" alt="">
+                        </div>
+                        <div class="galeri__photo-image">
+                            <img class="galeri__image" src="{{URL::asset('/images/dummy_galeri-3.jpg')}}" alt="">
+                        </div>
+                        <div class="galeri__photo-image">
+                            <img class="galeri__image" src="{{URL::asset('/images/dummy_galeri-4.jpg')}}" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection

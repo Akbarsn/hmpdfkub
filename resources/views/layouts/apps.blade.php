516f0caa8dbb7923260db568195a4372649e62ec
ranggadi <adirangga922@gmail.com>
2020-02-18 19:46:40 +0700

    [Rangga] feat: finishing up galeri pages

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -14,10 +14,11 @@
 <body>
     @yield('body')
     @include('inc.footer')
+    @yield('custom_script')
+    <script src="https://kit.fontawesome.com/5a91d5d98c.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 </body>
-<script src="https://kit.fontawesome.com/5a91d5d98c.js" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
 </html>

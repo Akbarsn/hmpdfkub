e73a71773610ce619f582f04899143a755ac9eef
Favian Pratista <farelony@gmail.com>
2020-02-23 03:30:43 +0700

    [Favian] Fixing Gallery Back End

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Gallery;
-use Gallery as GlobalGallery;
 use Symfony\Component\Console\Input\Input;
 
 class GalleryController extends Controller
